633575ca9d78d10d0f5cf7212858952f5d22cf31
Jure Šorn <sornjure@gmail.com>
2019-04-12 22:21:42 +0200

    JS script removes new from TOC if Menlo font not present

--- web/script_2.js
+++ web/script_2.js
@@ -46,8 +46,10 @@ const DIAGRAM_2_B =
 (function(d){function c(c){b.style.fontFamily=c;e.appendChild(b);f=b.clientWidth;e.removeChild(b);return f}var f,e=d.body,b=d.createElement("span");b.innerHTML=Array(100).join("wi");b.style.cssText=["position:absolute","width:auto","font-size:128px","left:-99999px"].join(" !important;");var g=c("monospace"),h=c("serif"),k=c("sans-serif");window.isFontAvailable=function(b){return g!==c(b+",monospace")||k!==c(b+",sans-serif")||h!==c(b+",serif")}})(document);
 
 if (!isFontAvailable('Menlo')) {
-    $(`code:contains(${DIAGRAM_1_B})`).html(DIAGRAM_1_A)
-    $(`code:contains(${DIAGRAM_2_B})`).html(DIAGRAM_2_A)
+    $(`code:contains(${DIAGRAM_1_B})`).html(DIAGRAM_1_A);
+    $(`code:contains(${DIAGRAM_2_B})`).html(DIAGRAM_2_A);
+    var htmlString = $('code:contains(ᴺᴱᵂ)').html().replace(/ᴺᴱᵂ/g, '');
+    $('code:contains(ᴺᴱᵂ)').html(htmlString);
 }
 
 
