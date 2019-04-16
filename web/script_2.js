2b9f30a4be136b3edebcd3e0192be88fd8a75c48
Jure Šorn <sornjure@gmail.com>
2019-04-16 21:19:15 +0200

    Button

--- web/script_2.js
+++ web/script_2.js
@@ -54,7 +54,7 @@ if (!isFontAvailable('Menlo')) {
 
 // ===== Scroll to Top ==== 
 $(window).scroll(function() {
-  if ($(this).scrollTop() >= 50) {      // If page is scrolled more than 50px
+  if ($(this).scrollTop() >= 590) {      // If page is scrolled more than 50px
     $('#return-to-top').fadeIn(200);    // Fade in the arrow
   } else {
     $('#return-to-top').fadeOut(200);   // Else fade out the arrow
