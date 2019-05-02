12c8c9b30b06457740dec2509e0fc87fa6f38b60
Jure Šorn <sornjure@gmail.com>
2019-05-02 17:37:19 +0200

    Font detection

--- web/script_2.js
+++ web/script_2.js
@@ -43,7 +43,8 @@ const DIAGRAM_2_B =
   '┗━━━━━━━━━┷━━━━━━━━━━━━━┛\n';
 
 
-// (function(d){function c(c){b.style.fontFamily=c;e.appendChild(b);f=b.clientWidth;e.removeChild(b);return f}var f,e=d.body,b=d.createElement("span");b.innerHTML=Array(100).join("wi");b.style.cssText=["position:absolute","width:auto","font-size:128px","left:-99999px"].join(" !important;");var g=c("monospace"),h=c("serif"),k=c("sans-serif");window.isFontAvailable=function(b){return g!==c(b+",monospace")||k!==c(b+",sans-serif")||h!==c(b+",serif")}})(document);
+// isFontAvailable:
+(function(d){function c(c){b.style.fontFamily=c;e.appendChild(b);f=b.clientWidth;e.removeChild(b);return f}var f,e=d.body,b=d.createElement("span");b.innerHTML=Array(100).join("wi");b.style.cssText=["position:absolute","width:auto","font-size:128px","left:-99999px"].join(" !important;");var g=c("monospace"),h=c("serif"),k=c("sans-serif");window.isFontAvailable=function(b){return g!==c(b+",monospace")||k!==c(b+",sans-serif")||h!==c(b+",serif")}})(document);
 
 if (!isFontAvailable('Menlo')) {
   $(`code:contains(${DIAGRAM_1_B})`).html(DIAGRAM_1_A);
