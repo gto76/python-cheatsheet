d62af13ac625edc2f3d24c9be5af90bdab04d250
Jure Šorn <sornjure@gmail.com>
2019-07-05 16:07:45 +0200

    Script_2.js diagrams test

--- web/script_2.js
+++ web/script_2.js
@@ -133,8 +133,8 @@ const DIAGRAM_6_B =
 (function(d){function c(c){b.style.fontFamily=c;e.appendChild(b);f=b.clientWidth;e.removeChild(b);return f}var f,e=d.body,b=d.createElement("span");b.innerHTML=Array(100).join("wi");b.style.cssText=["position:absolute","width:auto","font-size:128px","left:-99999px"].join(" !important;");var g=c("monospace"),h=c("serif"),k=c("sans-serif");window.isFontAvailable=function(b){return g!==c(b+",monospace")||k!==c(b+",sans-serif")||h!==c(b+",serif")}})(document);
 
 if (!isFontAvailable('Menlo')) {
-  $(`code:contains(${DIAGRAM_6_B})`).html(DIAGRAM_6_A);
   $(`code:contains(${DIAGRAM_1_B})`).html(DIAGRAM_1_A);
+  $(`code:contains(${DIAGRAM_6_B})`).html(DIAGRAM_6_A);
   $(`code:contains(${DIAGRAM_2_B})`).html(DIAGRAM_2_A);
   $(`code:contains(${DIAGRAM_3_B})`).html(DIAGRAM_3_A);
   $(`code:contains(${DIAGRAM_4_B})`).html(DIAGRAM_4_A);
