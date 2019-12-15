845e8999b4f17972120c5ccc30ee32ee7eb70450
Jure Šorn <sornjure@gmail.com>
2019-12-15 19:32:12 +0100

    Fixed mobile TOC orientation listener

--- web/script_2.js
+++ web/script_2.js
@@ -274,7 +274,7 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
     isMobile = true;
 }
 
-var TOC_SCREEN_WIDTH_CUTOFF = 400
+var TOC_SCREEN_WIDTH_CUTOFF = 667
 
 if (isMobile && window.screen.width < TOC_SCREEN_WIDTH_CUTOFF) {
     $(`code:contains(ToC)`).html(TOC_MOBILE);
